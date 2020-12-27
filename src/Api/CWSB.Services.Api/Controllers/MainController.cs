ef74bfd0124387e287b9eede39e8bd3d33777173
Douglas Mariano Silverio <douglas07@gmail.com>
2020-12-27 16:18:43 -0300

    Add project files.

---
+++ src/Api/CWSB.Services.Api/Controllers/MainController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CWSB.Services.Api.Controllers
+{
+    
+    [ApiController]    
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Errors = new List<string>();
+
+        protected ActionResult CustomResponse(object result=null)
+        {
+            if (ValidOperation())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                { "Messages",Errors.ToArray()}
+            })) ;
+        }
+
+        //todo - Add function to handle fail fast validations.
+
+        private bool ValidOperation()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void AddOperationError(string error)
+        {
+            Errors.Add(error);
+        }
+
+        protected void ClearOperationErrors()
+        {
+            Errors.Clear();
+        }
+
+
+
+
+    }
+}
