53bd74a0a5e6161dc4de21b522d02f8305c87ac0
Douglas Mariano Silverio <douglas07@gmail.com>
2021-01-06 17:48:50 -0300

    removed comments for Js file

--- src/Web/MVC/CWSB.WebApp.MVC/wwwroot/js/chat.js
+++ src/Web/MVC/CWSB.WebApp.MVC/wwwroot/js/chat.js
@@ -8,24 +8,11 @@ connection.on("ReceiveMessage", function (messages) {
     $("#messagesList").empty();
 
     
-    messages.forEach(function (m) {
-        //console.log(m);
-        //console.log(m.user);
-        //console.log(m.text);
-        //console.log(m.date);
+    messages.forEach(function (m) {        
         var li = $("<li></li>").text(m.date + " - " + m.user + ": " + m.text);        
         li.addClass("list-group-item");
         $("#messagesList").append(li);
     });
-
-
-    console.log(list);
-
-    /*var msg = message.replace(/&/g, "&").replace(/</g, "<").replace(/>/g, ">");
-    var li = $("<li></li>").text(when + " - " + user + ": " + msg);
-    console.log(when + " - " + user + ": " + msg);
-    li.addClass("list-group-item");
-    $("#messagesList").append(li);*/
 });
 
 connection.start().then(function () {
